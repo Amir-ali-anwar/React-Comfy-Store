83cd3b09d61429276a722e75c0e4544ad7f47a45
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-04-20 19:06:41 +0500

    setting up the multiple pages

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,9 +9,42 @@ import CartButtons from './CartButtons'
 import { useUserContext } from '../context/user_context'
 
 const Sidebar = () => {
-  return <h4>sidebar</h4>
+  // const { isSideBarOpen, sidebarClose } = useProductsContext()
+  // console.log(isSidebarOpen);
+  return (
+    <SidebarContainer>
+      <aside
+        
+      >
+        <div className='sidebar-header'>
+          <img src={logo} className='logo' alt='coding addict' />
+          <button className='close-btn' >
+            <FaTimes />
+          </button>
+        </div>
+        <ul className='links'>
+          {links.map(({ id, text, url }) => {
+            return (
+              <li key={id}>
+                <Link to={url} >
+                  {text}
+                </Link>
+              </li>
+            )
+          })}
+          {/* {myUser && (
+            <li>
+              <Link to='/checkout' onClick={closeSidebar}>
+                checkout
+              </Link>
+            </li>
+          )} */}
+        </ul>
+        <CartButtons />
+      </aside>
+    </SidebarContainer>
+  )
 }
-
 const SidebarContainer = styled.div`
   text-align: center;
   .sidebar-header {
