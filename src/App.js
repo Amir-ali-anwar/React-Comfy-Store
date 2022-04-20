665d1ceb9072ae054b0abeb3c4a4f0c519e8fe33
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-04-20 19:05:11 +0500

    working on setting up the contact page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// import { Navbar, Sidebar, Footer } from './components'
-import Home from './pages/HomePage'
-// import {
-//   Home,
-//   SingleProduct,
-//   Cart,
-//   Checkout,
-//   Error,
-//   About,
-//   Products,
-//   PrivateRoute,
-//   AuthWrapper,
-// } from './pages'
+import { Navbar, Sidebar, Footer } from './components'
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+  AuthWrapper,
+} from './pages'
 function App() {
   return (
     <Router>
-      {/* <Sidebar /> */}
-      <Switch>
-       <Route path='/'>
-       <Home />
-       </Route>
-      </Switch>
-    </Router>
+    <Navbar />
+    {/* <Sidebar /> */}
+    <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+      <Route path='/cart'>
+        <Cart />
+      </Route>
+      <Route exact path='/products'>
+        <Products />
+      </Route>
+      <Route path='/products/:id' children={<SingleProduct />}/>
+        <Route path='*'>
+        <Error />
+      </Route>
+    </Switch>
+    <Footer />
+  </Router>
   )
 }
 
