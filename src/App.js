afb33c7fff667593dff55c8622a7373fdf38cc1c
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-04-20 18:32:37 +0500

    setting up the hero page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
-
+// import { Navbar, Sidebar, Footer } from './components'
+import Home from './pages/HomePage'
+// import {
+//   Home,
+//   SingleProduct,
+//   Cart,
+//   Checkout,
+//   Error,
+//   About,
+//   Products,
+//   PrivateRoute,
+//   AuthWrapper,
+// } from './pages'
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      {/* <Sidebar /> */}
+      <Switch>
+       <Route path='/'>
+       <Home />
+       </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
