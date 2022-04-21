5f5a111fc358417548c0538f11b6373f4d63db75
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-04-21 21:56:47 +0500

    wip:working on single product page

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
       <Route exact path='/products'>
         <Products />
       </Route>
-      <Route path='/products/:id' children={<SingleProduct />}/>
+      <Route path='/products/:ProductId' children={<SingleProduct />}/>
         <Route path='*'>
         <Error />
       </Route>
