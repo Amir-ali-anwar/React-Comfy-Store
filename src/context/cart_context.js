5b71143595adcc4e5bb64e3f3ea7474a51e2939d
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-05-07 20:02:35 +0500

    setting up the cart content

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,17 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions'
 
+const getlocalStorageDdata=()=>{
+  let cart=localStorage.getItem('cart');
+  if(cart){
+    return JSON.parse(localStorage.getItem('cart'))
+  }
+  else{
+    return []
+  }
+}
 const initialState = {
-  cart:[],
+  cart:getlocalStorageDdata(),
   total_amount:0,
   total_items:0,
   free_shipping:false
@@ -20,11 +29,22 @@ const CartContext = React.createContext()
 export const CartProvider = ({ children }) => {
  const [state,dispatch]=useReducer(reducer,initialState)
  const addTocart=(id,amount,product,color)=>{
-  
   dispatch({type:ADD_TO_CART,payload:{id,amount,product,color}})
  }
+ const removeItem=(id)=>{
+
+ }
+ const toggleAmount=(id,value)=>{
+
+ }
+ const clearCart=()=>{
+
+ }
+ useEffect(() => {
+  localStorage.setItem('cart',JSON.stringify(state.cart))
+}, [state.cart])
   return (
-    <CartContext.Provider value={{...state,addTocart}}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{...state,addTocart,removeItem,toggleAmount,clearCart}}>{children}</CartContext.Provider>
   )
 }
 // make sure use
